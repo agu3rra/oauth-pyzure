20261cfb17e60ca0962ceafe76aef42b0d560535
agu3rra <agu3rra@gmail.com>
2021-04-03 09:54:07 -0300

    updating deployment version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='oauth-pyzure',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
-    install_requires=['requests==2.24.0','cryptography==3.2','pyjwt==1.7.1'],
+    install_requires=['requests==2.25.1','cryptography==3.4.7','pyjwt==2.0.1'],
     author='Andre Guerra',
     author_email='agu3rra@gmail.com',
     description='OAuth Pyzure: OAuth for Python with Azure',
